55442d911725aa569f0c777ad050cf5bcdc2a025
Flavia Venturini <flaviaventurini@msn.com>
2022-08-01 00:04:39 -0300

    Update

---
+++ Aula-UIViewController/Aula-UIViewController/ViewCode/AloneViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AloneViewController.swift
+//  Aula-UIViewController
+//
+//  Created by Flavia Venturini on 31/07/22.
+//
+
+import UIKit
+
+class AloneViewController: UIViewController {
+    let contentView: UIView = {
+        let view = UIView()
+        return view
+    }()
+}
